8e1fe29ec426489dcfcd99bcf7e3f382847ce7c0
speedkuk <speedkuk@gmail.com>
2011-11-16 17:43:52 +0900

    Store Procedure

--- src/main/java/com/mesplus/MBI/controller/MBIController.java
+++ src/main/java/com/mesplus/MBI/controller/MBIController.java
@@ -28,4 +28,55 @@ public class MBIController {
 		
 		return formDao.controlSqlGenNT(fac_id, func_id);
 	}
+	
+	@RequestMapping(value = "module/MBI/data/mapcon_gen_nt.json", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Map<String, Object>> mapConGenNt(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fac_id = request.getParameter("fac_id");
+		String func_id = request.getParameter("func_id");
+		String lang_flag = request.getParameter("lang_flag");
+		
+		return formDao.mapConGenNtDao(fac_id, func_id, lang_flag);
+	}
+	
+	@RequestMapping(value = "module/MBI/data/mapdef_s2_nt.json", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Map<String, Object>> mapDefS2Nt(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fac_id = request.getParameter("fac_id");
+		String func_id = request.getParameter("func_id");
+		String admin_user = request.getParameter("admin_user");
+		
+		return formDao.mapDefS2NtDao(fac_id, func_id, admin_user);
+	}
+	
+	@RequestMapping(value = "module/MBI/data/tabvld_nt.json", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Map<String, Object>> tabVldNt(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fac_id = request.getParameter("fac_id");
+		String func_id = request.getParameter("func_id");
+		String spd_id = request.getParameter("spd_id");
+		
+		return formDao.tabVldNtDao(fac_id, func_id, spd_id);
+	}
+	
+	@RequestMapping(value = "module/MBI/data/usrcol_nt.json", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Map<String, Object>> usrColNt(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fac_id = request.getParameter("fac_id");
+		String func_id = request.getParameter("func_id");
+		String grp_usr_id = request.getParameter("grp_usr_id");
+		String lang_flag = request.getParameter("lang_flag");
+		
+		return formDao.usrColNtDao(fac_id, func_id, grp_usr_id, lang_flag);
+	}
+	
+	@RequestMapping(value = "module/MBI/data/usrmap_nt.json", method = RequestMethod.GET)
+	public @ResponseBody
+	List<Map<String, Object>> usrMapNt(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String fac_id = request.getParameter("fac_id");
+		String func_id = request.getParameter("func_id");
+		String grp_usr_id = request.getParameter("grp_usr_id");
+		
+		return formDao.usrMapNtDao(fac_id, func_id, grp_usr_id);
+	}
 }
--- src/main/java/com/mesplus/MBI/dao/impl/JdbcFormDaoImpl.java
+++ src/main/java/com/mesplus/MBI/dao/impl/JdbcFormDaoImpl.java
@@ -35,4 +35,59 @@ public class JdbcFormDaoImpl implements FormDao {
 		
 		return (List<Map<String, Object>>)results.get(ControlSQLProcedure.CUR_REFER_PARAM);
 	}
+	
+	public List<Map<String, Object>> mapConGenNtDao(String fac_id, String func_id, String lang_flag) throws SQLException {
+		if (fac_id == null || func_id == null || lang_flag == null) {
+            throw new IllegalArgumentException("Parameters(fac_id, func_id, lang_flag) should not be null.");
+        }
+		
+		MapConGenNt sp = new MapConGenNt(dataSource);
+		Map<String, Object> results = sp.execute(fac_id, func_id, lang_flag);
+		
+		return (List<Map<String, Object>>)results.get(MapConGenNt.CUR_REFER_PARAM);
+	}
+	
+	public List<Map<String, Object>> mapDefS2NtDao(String fac_id, String func_id, String admin_user) throws SQLException {
+		if (fac_id == null || func_id == null || admin_user == null) {
+            throw new IllegalArgumentException("Parameters(fac_id, func_id, admin_user) should not be null.");
+        }
+		
+		MapDefS2Nt sp = new MapDefS2Nt(dataSource);
+		Map<String, Object> results = sp.execute(fac_id, func_id, admin_user);
+		
+		return (List<Map<String, Object>>)results.get(MapDefS2Nt.CUR_REFER_PARAM);
+	}
+	
+	public List<Map<String, Object>> tabVldNtDao(String fac_id, String func_id, String spd_id) throws SQLException {
+		if (fac_id == null || func_id == null || spd_id == null) {
+            throw new IllegalArgumentException("Parameters(fac_id, func_id, spd_id) should not be null.");
+        }
+		
+		TabVldNt sp = new TabVldNt(dataSource);
+		Map<String, Object> results = sp.execute(fac_id, func_id, spd_id);
+		
+		return (List<Map<String, Object>>)results.get(TabVldNt.CUR_REFER_PARAM);
+	}
+	
+	public List<Map<String, Object>> usrColNtDao(String fac_id, String func_id, String grp_usr_id, String lang_flag) throws SQLException {
+		if (fac_id == null || func_id == null || grp_usr_id == null || lang_flag == null) {
+            throw new IllegalArgumentException("Parameters(fac_id, func_id, grp_usr_id, lang_flag) should not be null.");
+        }
+		
+		UsrColNt sp = new UsrColNt(dataSource);
+		Map<String, Object> results = sp.execute(fac_id, func_id, grp_usr_id, lang_flag);
+		
+		return (List<Map<String, Object>>)results.get(UsrColNt.CUR_REFER_PARAM);
+	}
+	
+	public List<Map<String, Object>> usrMapNtDao(String fac_id, String func_id, String grp_usr_id) throws SQLException {
+		if (fac_id == null || func_id == null || grp_usr_id == null) {
+            throw new IllegalArgumentException("Parameters(fac_id, func_id, grp_usr_id) should not be null.");
+        }
+		
+		UsrMapNt sp = new UsrMapNt(dataSource);
+		Map<String, Object> results = sp.execute(fac_id, func_id, grp_usr_id);
+		
+		return (List<Map<String, Object>>)results.get(UsrMapNt.CUR_REFER_PARAM);
+	}
 }
