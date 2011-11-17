ebf2d0b06b15708e380cc6fbe713c6a8aaab59a7
shnam <shnam2k@gmail.com>
2011-11-17 20:48:46 +0900

    MBI view & stores

---
+++ src/main/webapp/resources/module/MBI/model/ConsqlGenNt.js
@@ -0,0 +1,14 @@
+Ext.define('MBI.model.ConsqlGenNt', {
+	fields: [
+	{ name: 'left_col', type: 'string' },
+	{ name: 'left_dsgn_id', type: 'string' },
+	{ name: 'left_prnt_key_flag', type: 'string' },
+	{ name: 'left_seq_no', type: 'number' },
+	{ name: 'right_col', type: 'string' },
+	{ name: 'right_dsgn_id', type: 'number' },
+	{ name: 'right_prnt_key_flag', type: 'string' },
+	{ name: 'right_seq_no', type: 'number' },
+	{ name: 'spread_id', type: 'number' },
+	{ name: 'spread_level', type: 'number' }
+	]
+});
