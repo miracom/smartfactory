d47c37ba0977b9bcc1a8953392c039b49097272b
shnam <shnam2k@gmail.com>
2011-11-17 19:18:46 +0900

    w/ Complex Store Test

---
+++ src/main/webapp/resources/module/MBI/view/FormDesign.js
@@ -0,0 +1,19 @@
+Ext.define('MBI.view.FormDesign', {
+	extend: 'Ext.grid.Panel',
+	
+	store : Ext.getStore('yyyyy'),
+	
+	columns : [ {
+		xtype : 'gridcolumn',
+		autoScroll : true,
+		dataIndex : 'sp_name',
+		align : 'center',
+		text : 'SP Name'
+	}, {
+		xtype : 'gridcolumn',
+		dataIndex : 'sp_id',
+		align : 'center',
+		text : 'SP ID'
+	} ]
+
+});
