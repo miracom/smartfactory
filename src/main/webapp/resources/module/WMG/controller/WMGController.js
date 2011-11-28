6a206ed90d45cbe4857f07ab01f6800772611b9e
shnam <shnam2k@gmail.com>
2011-11-28 18:01:11 +0900

    presence

--- src/main/webapp/resources/module/WMG/controller/WMGController.js
+++ src/main/webapp/resources/module/WMG/controller/WMGController.js
@@ -18,6 +18,7 @@ Ext.define('WMG.controller.WMGController', {
     		iconCls : 'iconsetDockCommunicator'
 		});
 		
+		var self = this;
 		SmartFactory.communicator(Ext.create('WMG.service.Communicator', {
 			username : SmartFactory.user(),
 			callback_notice : function(message) {
@@ -26,12 +27,44 @@ Ext.define('WMG.controller.WMGController', {
 				SmartFactory.msg(message.data.title, message.data.message);
 			},
 			callback_join_in : function(message) {
+				self.joinIn(message.data.username);
 				SmartFactory.msg('Joined in.', message.data.username);
 			},
 			callback_join_out : function(message) {
+				self.joinOut(message.data.username);
 				SmartFactory.msg('Joined out.', message.data.username);
 			}
 		})).join();
+	},
+	
+	joinIn : function(user) {
+		var store = Ext.getStore('WMG.store.CommunicatorStore');
+		
+		var idx = store.findExact('id', user);
+		if(idx !== -1) {
+			store.getAt(idx).set('status', 'on');
+		} else {
+			store.add({
+				id: user,
+				name: user,
+				status : 'on'
+			});
+		}
+	},
+
+	joinOut : function(user) {
+		var store = Ext.getStore('WMG.store.CommunicatorStore');
+		
+		var idx = store.findExact('id', user);
+		if(idx !== -1) {
+			store.getAt(idx).set('status', 'off');
+		} else {
+			store.add({
+				id: user,
+				name: user,
+				status : 'off'
+			});
+		}
 	}
 
 });
