e679f1cce51d900a5b5416fbf322a46238185a0f
shnam <shnam2k@gmail.com>
2011-12-01 14:34:11 +0900

    add cache management settings for ehcache. caching samples - menus and favorites.

--- src/main/java/com/mesplus/CMN/dao/MenuDao.java
+++ src/main/java/com/mesplus/CMN/dao/MenuDao.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.googlecode.ehcache.annotations.Cacheable;
 import com.mesplus.CMN.model.Menu;
 
 @Component
 public interface MenuDao {
+	// Since menu objects could be modified to hierarchy, do not cache menu dao results. (If you want cache, do it in controllers.)
 	List<Menu> selectMenus(Map<String, Object> params);
 }
