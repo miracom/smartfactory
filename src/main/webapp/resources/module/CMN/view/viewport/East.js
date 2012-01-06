35e7a64c09c828a50feff4573c3ec1556fc47f49
yhreset <yhreset@gmail.com>
2012-01-06 19:49:41 +0900

    ie support, etc style

--- src/main/webapp/resources/module/CMN/view/viewport/East.js
+++ src/main/webapp/resources/module/CMN/view/viewport/East.js
@@ -37,7 +37,7 @@ Ext.define('CMN.view.viewport.East', {
 //			console.log('enable');
 		}
 	},
-	
+		
 	/* 'east' 영역은 'base'라는 아이템의 View를 items에 가져야 한다.
 	 * 보조 패널을 갖지 않는 컨텐트 뷰가 Activate될 때, 이 'base'뷰가 activate된다.
 	 */
@@ -45,6 +45,6 @@ Ext.define('CMN.view.viewport.East', {
 	items : [{
 		xtype : 'box',
 		itemId : 'base',
-		html : '이 화면은 디폴트 화면입니다.'
+		cls : 'defaultSupplementImg'
 	}]
 });
