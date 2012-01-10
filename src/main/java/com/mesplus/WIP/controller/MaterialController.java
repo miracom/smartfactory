c6c884ad02187c585bd4279b8f90a9e13e717406
ace0hoon <ace0hoon.kim@miracom.co.kr>
2012-01-10 17:12:40 +0900

    Material Setup Query Samples

--- src/main/java/com/mesplus/WIP/controller/MaterialController.java
+++ src/main/java/com/mesplus/WIP/controller/MaterialController.java
@@ -59,3 +59,54 @@ public class MaterialController {
 		return materialDao.findMaterial(factory, mat_id, Integer.parseInt(mat_ver));
 	}
 }
+
+/*
+// Material Setup화면  
+// Radio Button 1번, 2번으로 선택 후 조회시 원본 쿼리 소스입니다.
+strSqlString.Append("SELECT MAT_ID, MAT_VER, MAT_DESC, DELETE_FLAG, DEACTIVE_FLAG \n");
+strSqlString.Append("  FROM MWIPMATDEF \n");
+strSqlString.AppendFormat(" WHERE FACTORY = '{0}' \n", sExtFactory == "" ? MPGV.gsFactory : sExtFactory);
+strSqlString.Append("   AND (MAT_ID, MAT_VER) IN (SELECT MAT_ID, MAX(MAT_VER) \n");
+strSqlString.Append("                               FROM MWIPMATDEF\n");
+strSqlString.AppendFormat("                              WHERE FACTORY = '{0}' \n", sExtFactory == "" ? MPGV.gsFactory : sExtFactory);
+strSqlString.AppendFormat("                                AND MAT_ID LIKE '{0}%' \n", sFilter);
+strSqlString.AppendFormat("                                AND MAT_TYPE LIKE '{0}%' \n", sMaterialType);
+strSqlString.AppendFormat("                                AND DELETE_FLAG LIKE '{0}%' \n", cDeleteFlag);
+strSqlString.AppendFormat("                                AND DEACTIVE_FLAG LIKE '{0}%' \n", cDeactiveFlag);
+strSqlString.Append("                              GROUP BY MAT_ID) \n");
+strSqlString.Append(" ORDER BY MAT_ID \n");
+
+
+//예제 샘플, 210.124.36.84번 DB(두산인프라코어 소주 공장 DB)로 실행하시면 결과를 보실 수 있습니다.
+SELECT MAT_ID, MAT_VER, MAT_DESC, DELETE_FLAG, DEACTIVE_FLAG 
+FROM MWIPMATDEF 
+WHERE FACTORY = '2510' 
+ AND MAT_ID LIKE '%' 
+ AND MAT_TYPE LIKE '%' 
+ AND DELETE_FLAG LIKE ' %' 
+ AND DEACTIVE_FLAG LIKE ' %' 
+ORDER BY MAT_ID ASC, MAT_VER DESC
+
+
+
+// Radio Button 3번으로 선택 후 조회시 원본 쿼리 소스입니다.
+strSqlString.Append("SELECT MAT_ID, MAT_VER, MAT_DESC, DELETE_FLAG, DEACTIVE_FLAG \n");
+strSqlString.Append("  FROM MWIPMATDEF \n");
+strSqlString.AppendFormat(" WHERE FACTORY = '{0}' \n", sExtFactory == "" ? MPGV.gsFactory : sExtFactory);
+strSqlString.AppendFormat("   AND MAT_ID LIKE '{0}%' \n", sFilter);
+strSqlString.AppendFormat("   AND MAT_TYPE LIKE '{0}%' \n", sMaterialType);
+strSqlString.AppendFormat("   AND DELETE_FLAG LIKE '{0}%' \n", cDeleteFlag);
+strSqlString.AppendFormat("   AND DEACTIVE_FLAG LIKE '{0}%' \n", cDeactiveFlag);
+strSqlString.Append("  ORDER BY MAT_ID ASC, MAT_VER DESC");
+
+
+//예제 샘플, 210.124.36.84번 DB(두산인프라코어 소주 공장 DB)로 실행하시면 결과를 보실 수 있습니다.
+SELECT MAT_ID, MAT_VER, MAT_DESC, DELETE_FLAG, DEACTIVE_FLAG 
+FROM MWIPMATDEF 
+WHERE FACTORY = '2510' 
+ AND MAT_ID LIKE '%' 
+ AND MAT_TYPE LIKE '%' 
+ AND DELETE_FLAG LIKE ' %' 
+ AND DEACTIVE_FLAG LIKE ' %' 
+ORDER BY MAT_ID ASC, MAT_VER DESC;
+*/
